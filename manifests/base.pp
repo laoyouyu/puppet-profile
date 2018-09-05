8fcbfdbeff8a06e7b8a02de4a1c6a6c47b74b35e
root <root@laoyouyu4.mylabserver.com>
2018-09-06 03:26:50 +1000

    update 1

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,10 +2,7 @@ class profile::base(
  Array $ntp_servers,
 )
 {
-#  class {'::ntp':
-#    service_enable => $service_enable,
-# }
-  class {'::ntp':
+  class { '::ntp':
     servers => $ntp_servers,
  }
 }
